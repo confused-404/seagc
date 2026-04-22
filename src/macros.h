3b1e1b873a3e5417b679df24c8fd9d4721591be2
Yves Kloeckner <hosta.gruen@gmail.com>
2026-04-21 23:04:13 -0600

    make alignments size_t specific

--- src/macros.h
+++ src/macros.h
@@ -6,8 +6,10 @@
 #define MB(x) ((size_t) (x) * 1024ull * 1024ull)
 #define GB(x) ((size_t) (x) * 1024ull * 1024ull * 1024ull)
 
-#define ALIGN_UP(x, a) (((x) + ((a) - 1)) & ~((a) - 1))
-#define ALIGN_DOWN(x, a) ((x) & ~((a) - 1))
+#define ALIGN_UP(x, a) \
+  ((((size_t) (x)) + (((size_t) (a)) - 1u)) & ~(((size_t) (a)) - 1u))
+#define ALIGN_DOWN(x, a) \
+  (((size_t) (x)) & ~(((size_t) (a)) - 1u))
 
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
